ef23878a2684bd8b5f25c304013e01c8152f3806
WaterKat <WaterKatGames@gmail.com>
2019-04-20 16:13:04 -0700

    Added Reflection Script

---
+++ BeachHacksReflection/Assets/Scr_ReflectionTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Scr_ReflectionTest : MonoBehaviour
+{
+    public GameObject Object1;
+    public GameObject Object2;
+
+    void FixedUpdate()
+    {
+        Vector3 Object1Position = Object1.transform.position;
+        Object1Position.x *= -1f;
+        Object2.transform.position = Object1Position;
+
+        Quaternion Object1Rotation = Object1.transform.rotation;
+        Object1Rotation.x *= -1;
+        Object1Rotation.w *= -1;
+        Object2.transform.rotation = Object1Rotation;
+    }
+}
