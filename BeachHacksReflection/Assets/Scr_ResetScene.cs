8d025479bcee8a1b666f09a837d3dd477f4f04f3
WaterKat <WaterKatGames@gmail.com>
2019-04-21 09:58:12 -0700

    AddedSceneReset and MouseLock

--- BeachHacksReflection/Assets/Scr_ResetScene.cs
+++ BeachHacksReflection/Assets/Scr_ResetScene.cs
@@ -5,8 +5,25 @@ using UnityEngine;
 
 public class Scr_ResetScene : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
     private void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+
         if (Input.GetKey("r"))
         {
                 TimerManager.main.AddTask(LoadScene, 1.1f);
