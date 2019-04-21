852cb3a001672e01e55887e0224ed5b31f3fb29f
XZ-Crazy <pallittoxz@gmail.com>
2019-04-21 13:47:09 -0700

    Resolved conflict

--- BeachHacksReflection/Assets/NextLevel.cs
+++ BeachHacksReflection/Assets/NextLevel.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-<<<<<<< HEAD
+//<<<<<<< HEAD
     public string TargetLevel="1";
-=======
+//=======
     public int TargetLevel=1;
     public string NextScene;
->>>>>>> ad06adb5b6f44395823942ed09854b79ca676f00
+//>>>>>>> ad06adb5b6f44395823942ed09854b79ca676f00
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Scr_PlayerCamera>() != null)
