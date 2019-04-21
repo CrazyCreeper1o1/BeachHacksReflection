dfed089be7d0b6fca8176264f9e92fd15a77402c
WaterKat <WaterKatGames@gmail.com>
2019-04-21 09:50:26 -0700

    Prefab door

---
+++ BeachHacksReflection/Assets/Scr_ResetScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Scr_ResetScene : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey("r"))
+        {
+                TimerManager.main.AddTask(LoadScene, 1.1f);
+
+                localFade.StartFadeOut();
+        }
+    }
+
+    public FadeScreen localFade;
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
